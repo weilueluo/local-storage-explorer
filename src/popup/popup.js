8463f869cbf272ff8443195b6a91660d28da4d8d
Weilue Luo <luoweilue@gmail.com>
2023-12-02 23:29:58 +0000

    feat: styles & footer

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,7 +1,30 @@
+// fetch("../manifest.json").then(json => json.text()).then(text => {
+//     browser.management.getPermissionWarningsByManifest(text, warnings => {
+//         console.log("warnings")
+//         console.log(warnings)
+//     })
+// })
+
+async function getCurrentTab() {
+    const tabs = await browser.tabs.query({ active: true, currentWindow: true });
+    const tab = tabs[0];
+    return tab
+}
+
+async function getCurrentOrigin() {
+    const tab = await getCurrentTab()
+    let origin = new URL(tab.url).origin
+    if (!origin.endsWith("/")) {
+        origin += "/"  // requestPermission requires this forward slash to work
+    }
+    return origin
+}
+
+
 
 const searchButton = document.querySelector("#search_button")
 const searchInput = document.querySelector("#search_input")
-
+const content_div = document.querySelector("#content")
 
 // feat: whenever user start typing, focus input and do search
 let searchDelayTimeoutId = null;
@@ -14,25 +37,27 @@ function doSearchAfterDelay() {
     }, 100);
 }
 
-document.addEventListener("keypress", (e) => {
-    const isEnterKey = e.code === "Enter"
-    if (isEnterKey) {
-        searchButton.click();
-        return;
-    }
+// document.addEventListener("keyup", (e) => {
 
-    const isInput = () => {
-        return document.activeElement.nodeName === "INPUT"
-    }
+//     // console.log(e);
+//     // console.log(document.activeElement);
 
-    if (document.activeElement != searchInput && !isInput()) {
-        // type anywhere to focus on search input and start typing in it
-        e.preventDefault()
-        searchInput.focus()
-        searchInput.value += e.key
-        doSearchAfterDelay();  // we are changing input directly, wouldnt fire event, so trigger it manually
-    }
-})
+//     if (e.shiftKey || e.ctrlKey || e.metaKey || e.altKey || e.repeat) {
+//         return;
+//     }
+//     if (["INPUT", "TEXTAREA", "SUMMARY"].includes(document.activeElement.nodeName)) {
+//         return;
+//     }
+    
+//     if (document.activeElement != searchInput) {
+//         // type anywhere to focus on search input and start typing in it
+//         e.preventDefault()
+//         searchInput.focus()
+//         document.dispatchEvent(e)
+//         // searchInput.value += e.key
+//         // doSearchAfterDelay();  // we are changing input directly, wouldnt fire event, so trigger it manually
+//     }
+// })
 searchInput.addEventListener("input", (e) => {
     doSearchAfterDelay()
 })
@@ -67,30 +92,97 @@ async function getMaxDepth() {
 // feat: search through local storage
 searchButton.onclick = async function () {
     searchButton.disabled = true
-    document.body.classList.add("loading")
+    content_div.classList.add("loading")
     searchButton.classList.add("loading")
 
     const searchText = searchInput.value
+    // console.log("searching", searchText);
     const parsed = parseRecursive(await getLocalStorageContent(), await getMaxDepth())
     const trimmed = trimNotContainInPlace(parsed, searchText)
-    await refresh(trimmed)
+    // console.log("trimmed", trimmed);
+
+    await refreshUI(trimmed)
 
     searchButton.classList.remove("loading")
-    document.body.classList.remove("loading")
+    content_div.classList.remove("loading")
     searchButton.disabled = false
 }
 
+// re-create html tree with given node
+async function refreshUI(parsedNode) {
+
+    deleteHtmlContainerIn(content_div)
+
+    const lastUpdated = new Date()
+
+    insertStats(parsedNode, lastUpdated)
+
+    const inserted = insertHtmlRecursive(parsedNode, content_div, await getMaxDepth())
+
+    if (inserted === null) {
+        document.querySelector("#empty_label").classList.remove("display_none")
+    } else {
+        document.querySelector("#empty_label").classList.add("display_none")
+    }
+}
+
+// add stats
+function insertStats(node, lastUpdated) {
+    const statsNode = document.querySelector("#stats")
+    let entries, size;
+    if (!node) {
+        entries = "empty"
+        size = 0
+    } else {
+        entries = Object.keys(node.children).length
+
+        const blob = new Blob([node.clipboard_value], {
+            type: "application/json",
+        })
+        size = toHumanSize(blob.size)
+    }
+    
+    // console.log("blob", blob);
+    statsNode.innerHTML = `${entries} entries of size ${size} at ${toHumanDate(lastUpdated)}`
+}
+function toHumanSize(kb) {
+    if (kb >= 1000_000) {
+        return `${(kb / 1000_000).toFixed(2)} GB`
+    } else if (kb >= 1000) {
+        return `${(kb / 1000).toFixed(2)} MB`
+    } else {  // (kb < 1000)
+        return `${kb.toFixed(2)} KB`
+    }
+}
+function pad(any, pad, size) {
+    let s = new String(any)
+    while (s.length < size) {
+        s = pad + s
+    }
+    return s
+}
+function toHumanDate(date) {
+    return `${pad(date.getHours(), 0, 2)}:${pad(date.getMinutes(), 0, 2)}:${pad(date.getSeconds(), 0, 2)}`
+}
+
+
 function trimNotContainInPlace(node, text) {
+    // console.log("trim text", text);
+    // console.log("trim node", node);
     if (text === "") {
+        // console.log("returning node text empty");
         return node
     }
     if (!node) {
+        // console.log("returning null");
         return null;
     }
     if (node.is_leaf) {
         if (node.clipboard_value && node.clipboard_value.toLowerCase().includes(text.toLowerCase())) {
+            // console.log("returning node is leaf");
             return node
         } else {
+            // console.log("returning null leaf no match");
             return null
         }
     } else {
@@ -110,19 +202,21 @@ function trimNotContainInPlace(node, text) {
         if (Object.keys(trimmedChildren).length > 0) {
             // any of the children contains the text
             node.children = trimmedChildren
+            // console.log("returning node children not empty");
             return node
         } else {
+            // console.log("returning null children empty");
             return null;
         }
     }
 }
 
-// feat: refresh
+// feat: refresh, this is same as click
 const refreshButton = document.querySelector("#refresh_button")
 refreshButton.onclick = async function () {
     refreshButton.disabled = true
     refreshButton.classList.add("loading")
-    await refresh()
+    await searchButton.click()
     refreshButton.classList.remove("loading")
     refreshButton.disabled = false
 }
@@ -148,8 +242,7 @@ export function getAsNumber(anything) {
 }
 
 async function getSettings() {
-    const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
-    const origin = new URL(tab.url).origin
+    const origin = await getCurrentOrigin()
     const settings = await browser.storage.sync.get(origin)
     // console.log(`getting settings:`, settings[origin])
     return (settings && settings[origin]) ? settings[origin] : {}
@@ -163,45 +256,18 @@ async function saveSettings(settings) {
     if (!isObject(settings)) {
         throw new Error(`trying to save a non-object settings: ${settings}`)
     }
-    const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
-    const origin = new URL(tab.url).origin
+    const origin = await getCurrentOrigin()
     const toSave = {}
     toSave[origin] = settings
     await browser.storage.sync.set(toSave)
 }
 
-// feat: on open extension popup, get local storage content and create tree
-async function refresh(parsedNode) {
-
-    const maxDepth = await getMaxDepth()
-
-    if (parsedNode === null || parsedNode === undefined) {
-        parsedNode = parseRecursive(await getLocalStorageContent(), maxDepth)
-    }
-
-    deleteHtmlContainerIn(document.body)
-
-    const lastUpdated = new Date()
-
-    insertStats(parsedNode, lastUpdated)
-
-    const inserted = insertHtmlRecursive(parsedNode, document.body, maxDepth)
-
-    if (inserted === null) {
-        document.querySelector("#empty_label").classList.remove("display_none")
-    } else {
-        document.querySelector("#empty_label").classList.add("display_none")
-    }
-}
 
 async function getLocalStorageContent() {
-    // const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
-    // const response = await browser.tabs.sendMessage(tab.id, { type: "retrieve" });
-    // return response
-
-    // Get the current tab
-    const tabs = await browser.tabs.query({ active: true, currentWindow: true });
-    const tab = tabs[0];
+    if (browser.scripting === undefined) {
+        return undefined
+    }
+    const tab = await getCurrentTab()
     const execution = await browser.scripting.executeScript({
         target: { tabId: tab.id },
         func: () => ({ ...localStorage }),
@@ -210,6 +276,10 @@ async function getLocalStorageContent() {
 }
 
 function parseRecursive(raw, depth) {
+    return parseRecursiveInternal(raw, depth, 0)
+}
+
+function parseRecursiveInternal(raw, max_depth, depth) {
 
     let is_object = false
     let is_object_tag = false  // node maybe an object but not a object tag, because leaf node (max depth reached) always display full content, even if it is an object 
@@ -274,19 +344,21 @@ function parseRecursive(raw, depth) {
         is_array_tag: is_array_tag,
         is_string_tag: is_string_tag,
         children: {},  // key map to node
+        has_parent: depth > 1,
+        depth: depth
     }
 
     // set is_leaf
-    if (depth > 0) {
+    if (depth < max_depth) {
         if (node.is_object) {
             node.is_leaf = false
             for (const [k, v] of Object.entries(node.js_value)) {
-                node.children[k] = parseRecursive(v, depth - 1)
+                node.children[k] = parseRecursiveInternal(v, max_depth, depth+1)
             }
         } else if (node.is_array) {
             node.is_leaf = false
             node.js_value.forEach((value, i) => {
-                node.children[i] = parseRecursive(value, depth - 1)
+                node.children[i] = parseRecursiveInternal(value, max_depth, depth+1)
             })
         }
     }
@@ -303,14 +375,6 @@ function parseRecursive(raw, depth) {
 }
 
 // UI relate functions
-
-function insertStats(node, lastUpdated) {
-    const statsNode = document.querySelector("#stats")
-    if (statsNode) {
-        statsNode.innerHTML = `<b>Time</b> ${lastUpdated}<br/>`
-        statsNode.innerHTML += `<b>Items</b> ${Object.keys(node.children).length}<br/>`
-    }
-}
 
 function insertHtmlRecursive(node, parent, depth) {
     if (depth <= 0) {
@@ -349,10 +413,10 @@ function createHtmlNode(key, node) {
             })
     }
 
-    const visibilityElement = nodeContainer.querySelector(".visibility");
-    visibilityElement.onclick = async () => {
-        nodeContainer && nodeContainer.classList.toggle("hide_children")
-    }
+    // const visibilityElement = nodeContainer.querySelector(".visibility");
+    // visibilityElement.onclick = async () => {
+    //     nodeContainer && nodeContainer.classList.toggle("hide_children")
+    // }
 
     const keyElement = nodeContainer.querySelector(".key");
     keyElement.textContent = key
@@ -384,6 +448,11 @@ function createHtmlNode(key, node) {
         valueElement.classList.toggle("trim")
     }
 
+    // const downRightElement = nodeContainer.querySelector(".down_right")
+    // if (node.has_parent) {
+    //     downRightElement.classList.remove("display_none")
+    // }
+
     if (node.is_object_tag) {
         valueElement.classList.add("object_tag")
     } else if (node.is_array_tag) {
@@ -407,4 +476,102 @@ function deleteHtmlContainerIn(parentElement) {
     ul && ul.remove()
 }
 
-await refresh()
+// permissions
+
+// async function checkScriptingPermission() {
+//     let granted = false;
+//     const origin = await getCurrentOrigin()
+//     await browser.permissions.getAll().then(permissions => {
+//         console.log("permission", permissions);
+//         let originListed = false
+//         for (const listedOrigin of permissions.origins) {
+//             if (listedOrigin.startsWith(origin)) {
+//                 originListed = true;
+//                 break
+//             }
+//         }
+//         if (originListed) {
+//             granted = permissions.permissions.includes('scripting')
+//         }
+//     })
+
+//     // does not work in chrome
+//     // await browser.permissions.contains({
+//     //     permissions: ['scripting'],
+//     //     origins: [origin]
+//     // }, (result) => {
+//     //     granted = result
+//     // });
+
+//     return granted
+// }
+
+const access_div = document.querySelector("#access_div")
+async function checkPermission() {
+    access_div.classList.remove("display_none")
+
+    const origin = await getCurrentOrigin()
+    // reject all non webpage url
+    if (!origin.startsWith("https://") && !origin.startsWith("http://")) {
+        document.querySelector("#access_hint").textContent = `Cannot access non http/https webpage`
+        access_button.remove(); // remove access button because cant request access for this page
+    }
+
+    // const granted = await checkScriptingPermission()
+    const granted = (await getLocalStorageContent()) !== undefined  // as long as we get local storage successfully, then we have permissions
+    // console.log("getLocalStorageContent", getLocalStorageContent());
+    // console.log("check permission", granted);
+    if (granted) {
+        access_div.classList.add("display_none")
+        return true
+    } else {
+        const origin = await getCurrentOrigin()
+        document.querySelector("#access_text").textContent = `No permission to access ${origin}`
+        return false
+    }
+}
+
+// feat: request access
+
+const origin = await getCurrentOrigin(); // this must be placed outside of the handler for request permissions to work, no idea
+const access_button = document.querySelector("#access_button")
+access_button.addEventListener('click', async e => {
+    access_button.disabled = true
+    access_button.classList.add("loading")
+
+    // setTimeout(() => { // the extension popup may be blocking the browser request permission popup
+    //     window.close()
+    // }, 500);
+    console.log("origin", origin);
+    console.log("browser.permissions", browser.permissions);
+    console.log("browser.permissions.request", browser.permissions.request);
+
+    setTimeout(() => {
+        window.close()
+    }, 100);
+    await browser.permissions.request({
+        permissions: ["scripting"],
+        origins: [origin]
+    }); // chrome does not have second argument callback
+
+    const granted = checkPermission()
+    if (granted) {
+        searchButton.click()
+    }
+
+    access_button.classList.remove("loading")
+    access_button.disabled = false
+});
+
+
+// close button
+document.querySelector("#close_button").addEventListener('click', event => window.close());
+
+(async function main() {
+    searchInput.focus()
+    await checkPermission().then(async granted => {
+        if (granted) {
+            await searchButton.click()
+        }
+    })
+})()
